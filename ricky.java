105cc8fde935b258077805a2b9fcccb511cfd382
rickyezra <ezraricky9@gmail.com>
2024-05-27 23:37:17 +0530

    second commit

---
+++ ricky.java
@@ -0,0 +1,9 @@
+Public Class Subtraction
+     Public static void main(string[] args)
+     {
+        int a=6;
+        int b=3;
+        int Difference = a - b ;
+        system.out.println("Subtration is" +Difference);
+     }
+.....
